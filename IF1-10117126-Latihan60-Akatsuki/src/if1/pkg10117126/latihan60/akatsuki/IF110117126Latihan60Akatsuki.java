a7bfa71ad8bccc1f346780100e10b128b4af2149
mawafaqu <63239367+mawafaqu@users.noreply.github.com>
2020-11-20 17:30:51 +0700

    Add files via upload

---
+++ IF1-10117126-Latihan60-Akatsuki/src/if1/pkg10117126/latihan60/akatsuki/IF110117126Latihan60Akatsuki.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10117126.latihan60.akatsuki;
+
+/**
+ *Nama  : Mawa Faqu Rochman
+ *Kelas : IF-1
+ *NIM   : 10117126
+ *Deskripsi Program : program ini berisi program yang berisikan karakter dari 
+ *                    dari serial Naruto
+ */
+public class IF110117126Latihan60Akatsuki {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String pakaian = "Pakaian hitam beserta corak awan merah";
+        String tujuan = "Menangkap Semua Jinchuriki";
+
+        AnggotaAkatsuki itachi = new AnggotaAkatsuki("Itachi Uchiha", "Konohagakure", "Kunai",
+                "Anbu Captain Missing Ninja", "Mangekyo Sharingan",
+                "Kehabisan Chakra", "Suzaku(Merah darah)",
+                true, pakaian, tujuan);
+        AnggotaAkatsuki kisame = new AnggotaAkatsuki("Kisame Hoshigaki", "Kirigakure", "Samehada",
+                "S-Rank Missing Ninja", "Shark-o-mation", "Bunuh Diri",
+                " Nanju(Bintang Selatan)", true, pakaian, tujuan);
+        AnggotaAkatsuki deidara = new AnggotaAkatsuki("Deidara", "Iwagakure", "Explosive Clay",
+                "S-Rank Missing Ninja", "Explosive Release", "Dibunuh oleh Sasuke Uchiha",
+                "Seiryū(Naga Biru/Hijau) ", true, pakaian, tujuan);
+        AnggotaAkatsuki sasori = new AnggotaAkatsuki("Sasori", "Sunagakure", "Hiruko",
+                "S-Rank Missing Ninja from Sunagakure's Puppet Brigade",
+                "Puppet Mastery", "Dibunuh oleh Sakura Haruno", "Gyokunyo(Virgo)",
+                true, pakaian, tujuan);
+        AnggotaAkatsuki kakuzu = new AnggotaAkatsuki("Kakuzu", "Takigakure", "Benang hitam pada dalam tubuhnya",
+                "Zonbi Konbi", "Dapat menggunakan ke lima elemen",
+                "Dibunuh oleh Naruto Uzumaki dengan bantuan Team 10 dan Kakashi Hatake ",
+                "Hokuto(Bintang Utara)", true, pakaian, tujuan);
+        AnggotaAkatsuki hidan = new AnggotaAkatsuki("Hidan", "Yugakure", "Sabit besar bermata tiga",
+                "Zonbi Konbi", "Immortality ", "Tidak diketahui atau disegel Shikamaru",
+                "Santai(Tiga Tingkatan)", true, pakaian, tujuan);
+        AnggotaAkatsuki orochimaru = new AnggotaAkatsuki("Orochimaru", "Konohagakure", "Ular",
+                "Legendary Sannin", "Kekuatan Ular Putih", "Dibunuh dan dihidupkan oleh Sasuke Uchiha",
+                "Sora(Langit)", false, pakaian, tujuan);
+
+        System.out.println("        Profile Akatsuki       ");
+        System.out.println("===============================");
+        System.out.println();
+        System.out.println("Identitas : " + itachi.getpakaian());
+        System.out.println("Tujuan : " + itachi.gettujuan());
+        System.out.println();
+
+        itachi.displayAnggota();
+        kisame.displayAnggota();
+        deidara.displayAnggota();
+        sasori.displayAnggota();
+        kakuzu.displayAnggota();
+        hidan.displayAnggota();
+        orochimaru.displayAnggota();
+    }
+
+}
---
+++ IF1-10117126-Latihan60-Akatsuki/src/if1/pkg10117126/latihan60/akatsuki/KlanAkatsuki.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10117126.latihan60.akatsuki;
+
+/**
+ *
+ * @author MAWA
+ */
+public class KlanAkatsuki {
+
+    protected String pakaian;
+    protected String tujuan;
+
+    public KlanAkatsuki(String pakaian, String tujuan) {
+        this.pakaian = pakaian;
+        this.tujuan = tujuan;
+    }
+
+    public String getpakaian() {
+        return pakaian;
+    }
+
+    public String gettujuan() {
+        return tujuan;
+    }
+}
